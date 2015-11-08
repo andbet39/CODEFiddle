3e0a0397b7a24b0f77a1d3e9f32a8cfde14ff771
Andrea Terzani <andrea.terzani@gmail.com>
2015-11-08 21:43:52 +0100

    first working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,39 @@
  * Controller of the codefiddleApp
  */
 angular.module('codefiddleApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+
+    $scope.html ="";
+    $scope.css="";
+    $scope.javascript="";
+
+    $scope.aceChanged = function(){
+      console.log('changed');
+      $scope.run();
+    };
+
+    $scope.run = function(){
+      var result ='<!DOCTYPE html>' +
+        '<html>' +
+        '<head lang="en"> <meta charset="UTF-8"> <title></title><style>'+$scope.css+'</style> </head> <body>'+
+        $scope.html +
+          '<script type="text/javascript">' +
+          $scope.javascript+
+          '</script>'
+        '</body></html>';
+
+
+
+      var iframe = document.getElementById('result');
+      var doc;
+
+      if(iframe.contentDocument) doc = iframe.contentDocument;
+      else if(iframe.contentWindow) doc = iframe.contentWindow.document;
+      else doc = iframe.document;
+
+      doc.open();
+      doc.writeln(result);
+      doc.close();
+    }
+
   });
